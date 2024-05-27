d06e9863af7e8287ecb715d262110143f7edbbb9
Jacinto Mba Cantero <jacmba@gmail.com>
2024-05-27 16:38:15 +0100

    Initial commit

---
+++ src/components/common/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => 
+  <div className="center">
+    <img 
+      src="https://fontmeme.com/permalink/210824/53a131e0396614b0fd10a53a4c44ce42.png"
+      alt="Jazflix logo"
+    />
+  </div>
+
+export default Logo;
