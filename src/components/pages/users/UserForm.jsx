56792a09b256f9a26e75d244607ad65683925507
Jacinto Mba Cantero <jacmba@gmail.com>
2024-05-29 13:00:16 +0100

    Complete user form implementation

--- src/components/pages/users/UserForm.jsx
+++ src/components/pages/users/UserForm.jsx
@@ -1,21 +1,72 @@
-import { Col, Form, FormGroup, Row } from "react-bootstrap"
+import { useState } from "react"
+import { Button, Col, Form, FormGroup, Row } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
 
-const UserForm = () => {
+const UserForm = ({
+  defaultName,
+  defaultEnabled,
+  submitLabel,
+  cb
+}) => {
+
+  const [username, setUsername] = useState(defaultName)
+  const [enabled, setEnabled] = useState(defaultEnabled === 'true')
+  const [validated, setValidated] = useState(false)
+
+  const navigate = useNavigate()
+
+  const buttonLabel = submitLabel || 'Submit'
+
+  const handleSubmit = evt => {
+    const form = evt.currentTarget
+    if (!form.checkValidity()) {
+      evt.preventDefault()
+      evt.stopPropagation()
+      setValidated(true)
+    } else {
+      cb(username, enabled)
+      navigate('/users')
+    }
+  }
+
+  const handleNameChange = evt => {
+    setUsername(evt.target.value)
+  }
+
+  const handleEnabledChange = evt => {
+    setEnabled(evt.target.checked)
+  }
 
   return (
     <div className="container mt-5" data-testid="user-form-container">
-      <Form>
+      <Form onSubmit={handleSubmit} validated={validated} noValidate>
         <Row className="mb-3">
           <FormGroup as={Col} className="mb-3" controlId="form-username">
             <Form.Label>User name (email address)</Form.Label>
-            <Form.Control type="email" placeholder="Enter email address" data-testid="user-name-input"/>
+            <Form.Control type="email"
+              placeholder="Enter your email address"
+              defaultValue={username}
+              required
+              onChange={handleNameChange}
+              data-testid="user-name-input"/>
+            <Form.Control.Feedback
+              type="invalid"
+              data-testid="user-name-feedback">
+              Enter a valid email address
+            </Form.Control.Feedback>
           </FormGroup>
 
           <FormGroup as={Col} className="mb-3" controlId="form-enabled">
             <Form.Label>Enabled</Form.Label>
-            <Form.Check type="switch" data-testid="user-enabled-input" />
+            <Form.Check type="switch"
+              checked={enabled}
+              onChange={handleEnabledChange}
+              data-testid="user-enabled-input" />
           </FormGroup>
         </Row>
+        <Button type="submit" data-testid="submit-button">
+          {buttonLabel}
+        </Button>
       </Form>
     </div>
   )
--- src/components/pages/users/UserForm.test.js
+++ src/components/pages/users/UserForm.test.js
@@ -1,7 +1,19 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import UserForm from "./UserForm"
+import { useNavigate } from "react-router-dom"
+
+jest.mock('react-router-dom')
+const navigate = jest.fn()
 
 describe('UserForm', () => {
+
+  beforeEach(()=> {
+    useNavigate.mockImplementation(() => navigate)
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
 
   it('should display main container', () => {
     render(<UserForm />)
@@ -12,10 +24,90 @@ describe('UserForm', () => {
     expect(container).toHaveClass('mt-5')
   })
 
-  it('should display form inputs', () => {
+  it('should display form inputs and submit button', () => {
     render(<UserForm />)
 
-    const usernameInput = screen.getByTestId('user-name-input')
-    const enabledInput = screen.getByTestId('user-enabled-input')
+    const usernameInput = screen.queryByTestId('user-name-input')
+    const enabledInput = screen.queryByTestId('user-enabled-input')
+    const button = screen.queryByTestId('submit-button')
+
+    expect(usernameInput).toBeInTheDocument()
+    expect(enabledInput).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+
+    expect(usernameInput).toHaveProperty('placeholder', 'Enter your email address')
+    expect(usernameInput).toHaveProperty('required', true)
+    expect(enabledInput).not.toBeChecked()
+    expect(button).toHaveClass('btn')
+    expect(button).toHaveClass('btn-primary')
+    expect(button.innerHTML).toBe('Submit')
+    expect(button).toHaveProperty('type', 'submit')
+  })
+
+  it('should have default value John Doe for username input', () => {
+    render(<UserForm defaultName="John Doe" />)
+
+    const input = screen.queryByTestId('user-name-input')
+    expect(input).toHaveValue('John Doe')
+  })
+
+  it('user should be enabled by default', () => {
+    render(<UserForm defaultEnabled="true" />)
+
+    const input = screen.queryByTestId('user-enabled-input')
+    expect(input).toBeChecked()
+  })
+
+  it('user validation feedback should be present', () => {
+    render(<UserForm />)
+
+    const feedback = screen.queryByTestId('user-name-feedback')
+    expect(feedback).toBeInTheDocument()
+    expect(feedback).toHaveClass('invalid-feedback')
+    expect(feedback.innerHTML).toBe('Enter a valid email address')
+  })
+  
+  it('submit button label should be "Test"', () => {
+    render(<UserForm submitLabel="Test" />)
+
+    const button = screen.queryByTestId('submit-button')
+    expect(button.innerHTML).toBe('Test')
+  })
+
+  it('should update value on username input change', () => {
+    render(<UserForm />)
+
+    const input = screen.getByTestId('user-name-input')
+    fireEvent.change(input, {target: {value: 'test@foo.bar'}})
+    expect(input).toHaveValue('test@foo.bar')
+  })
+
+  it('should invoke callback and navigate to users list', () => {
+    const callback = jest.fn()
+
+    render(<UserForm cb={callback} />)
+
+    const input = screen.getByTestId('user-name-input')
+    const slider = screen.getByTestId('user-enabled-input')
+    const button = screen.getByTestId('submit-button')
+
+    fireEvent.change(input, {target: {value: 'test@foo.bar'}})
+    fireEvent.click(slider)
+    fireEvent.click(button)
+
+    expect(callback).toHaveBeenCalledWith('test@foo.bar', true)
+    expect(navigate).toHaveBeenCalledWith('/users')
+  })
+
+  it('invalid form should not perform actions', () => {
+    const callback = jest.fn()
+
+    render(<UserForm defaultName="wrong-address" cb={callback} />)
+
+    const button = screen.getByTestId('submit-button')
+    fireEvent.click(button)
+
+    expect(callback).not.toHaveBeenCalled()
+    expect(navigate).not.toHaveBeenCalled()
   })
 })
